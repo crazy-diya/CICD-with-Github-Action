4d787975986b7ef05effc47e8bb610de7cff12f1
Dimuthu Lakshan <dimuthu_l@epiclanka.net>
2024-01-21 12:10:40 +0530

    "git commit with CICD"

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cicd_github_action/main.dart';
+import 'package:cicdgithubaction/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
